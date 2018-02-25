3c27a0b33820b0a5b188e4e64ea8132eac99a369
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-02-25 00:07:16 -0600

    Added useless code

---
+++ gamemode/server/hooks.lua
@@ -0,0 +1,4 @@
+function GM:Initialize()
+    MsgC(Color(60, 60, 255), "[Fusion RP] - Successfully initialized version" .. GAMEMODE.Version)
+    MsgC(Color(60, 60, 255), "A gamemode by " .. GAMEMODE.Author)
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -6,3 +6,6 @@ PLAYER = FindMetaTable("Player")
 ENTITY = FindMetaTable("Entity")
 
 FUSION = FUSION or {}
+
+/* You must change this in order to use a different folder name throughout the gamemode. */
+FUSION.RootFolder = "fusionrp"
