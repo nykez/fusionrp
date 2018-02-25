9c4ccc9e0363eb63fda44e68952a866f3b0e93c4
nykeztv <nykezgaming@live.com>
2018-02-25 18:30:20 -0500

    Wrapper for includes (ez file load)

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -9,3 +9,26 @@ FUSION = FUSION or {}
 
 /* You must change this in order to use a different folder name throughout the gamemode. */
 FUSION.RootFolder = "fusionrp"
+
+
+local function client(file)
+	if SERVER then AddCSLuaFile(file) end
+
+	if CLIENT then
+		return include(file)
+	end
+end
+
+local function server(file)
+	if SERVER then
+		return include(file)
+	end
+end
+
+local function shared(file)
+	return client(file) or server(file)
+end
+
+
+// Usage:
+// shared("myfile/myfile.lua")
