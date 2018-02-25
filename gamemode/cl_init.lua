f6a749b8be9ab8bb9ac40dab9b25b13c814af4ab
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-02-25 17:34:19 -0600

    Reorganized gamemode structure and added core file inclusion

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,1 +1,14 @@
 include("shared.lua")
+
+/* Load core componenets */
+local root = GM.FolderName .. "/gamemode/core/"
+local cl_files = file.Find(root .. "cl_*.lua", "LUA")
+local sh_files = file.Find(root .. "sh_*.lua", "LUA")
+
+for k, v in pairs(cl_files) do
+    include(root .. v)
+end
+
+for k, v in pairs(sh_files) do
+    include(root .. v)
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,10 +2,10 @@ GM.Name = "Fusion RP"
 GM.Author = "Dark Fusion Gaming"
 GM.Version = "0.1"
 
+/* You must change this in order to use a different folder name throughout the gamemode. */
+GM.FolderName = "fusionrp"
+
 PLAYER = FindMetaTable("Player")
 ENTITY = FindMetaTable("Entity")
 
 FUSION = FUSION or {}
-
-/* You must change this in order to use a different folder name throughout the gamemode. */
-FUSION.RootFolder = "fusionrp"
