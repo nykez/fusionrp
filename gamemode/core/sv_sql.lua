e11c6e44079db54248b68f5c89d164351f1e047b
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-02-25 20:14:34 -0600

    SQL test

--- gamemode/core/sv_sql.lua
+++ gamemode/core/sv_sql.lua
@@ -18,3 +18,18 @@ local function connectDatabase()
 	mysql:Connect(mysql_data.hostname, mysql_data.username, mysql_data.password, mysql_data.database, mysql_data.port);
 end
 hook.Add( "Initialize", "fusion_connectDatabase", connectDatabase)
+
+local function discDatabase()
+	mysql:Disconnect()
+end
+hook.Add("Shutdown", "fusion_discDatabase", discDatabase)
+
+local function populateTables()
+	local players = mysql:Create("fusion_players")
+	players:Create("id", "VARCHAR(255) NOT NULL")
+	players:Create("rp_first", "VARCHAR(255) NOT NULL")
+	players:Create("rp_last", "VARCHAR(255) NOT NULL")
+	players:PrimaryKey("id")
+	players:Execute()
+end
+hook.Add("DatabaseConnected", "fusion_populateTables", populateTables)
