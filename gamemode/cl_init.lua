f13a5a772ad1f272c2085079360e399ef87b0ed1
nykeztv <nykezgaming@live.com>
2018-02-26 18:41:35 -0500

    module loader

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,2 +1,14 @@
 include("shared.lua")
 include("config.lua")
+
+local fil, fol = file.Find(GM.FolderName .. "/gamemode/modules/*", "LUA")
+
+for _, Dir in pairs(fol) do
+	for sh, File in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/sh_*.lua", "LUA")) do
+		include(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/" .. File)
+	end
+
+	for cl, File in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/cl_*.lua", "LUA")) do
+		include(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/" .. File)
+	end
+end
--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,3 +4,21 @@ AddCSLuaFile("config.lua")
 
 include("shared.lua")
 include("config.lua")
+
+
+local fil, fol = file.Find(GM.FolderName .. "/gamemode/modules/*", "LUA")
+
+for _, Dir in pairs(fol) do
+	for cl, File in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/cl_*.lua", "LUA")) do
+		AddCSLuaFile(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/" .. File)
+	end
+
+	for sh, File in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/sh_*.lua", "LUA")) do
+		AddCSLuaFile(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/" .. File)
+		include(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/" .. File)
+	end
+
+	for sv, File in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/sv_*.lua", "LUA")) do
+		include(GM.FolderName .. "/gamemode/modules/" .. Dir .. "/" .. File)
+	end
+end
