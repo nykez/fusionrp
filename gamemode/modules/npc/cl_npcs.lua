f858eb33006591f73f4a5b60a318270d6b1a909a
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-03-16 12:59:04 -0500

    Added crap for rockford

--- gamemode/modules/npc/cl_npcs.lua
+++ gamemode/modules/npc/cl_npcs.lua
@@ -1,11 +1,9 @@
 Fusion.npcs = Fusion.npcs or {}
 Fusion.npcs.cache = Fusion.npcs.cache or {}
 
-net.Receive("Fusion.npc.use", function(len, ply)
-    if !IsValid(ply) then return end
-
+net.Receive("Fusion.npc.use", function(len)
     local id = net.ReadInt(16)
     if !Fusion.npcs.cache[id] then return end
-    
+
     Fusion.npcs.cache[id].use()
 end)
--- gamemode/modules/npc/npcs/car_spawner.lua
+++ gamemode/modules/npc/npcs/car_spawner.lua
@@ -10,17 +10,25 @@ npc.model = "models/Humans/Group01/Female_04.mdl"
 npc.spawn = {
 	["gm_construct"] = {
 		Vector(491.350525, -680.441162, -145)
+	},
+
+	["rp_rockford_v2b"] = {
+		Vector(-4198, -669, 0)
 	}
 }
 
 npc.angle = {
 	["gm_construct"] = {
 		Angle(0, 90, 0)
+	},
+
+	["rp_rockford_v2b"] = {
+		Angle(0, -90, 0)
 	}
 }
 
 npc.use = function()
-
+	LocalPlayer():ChatPrint("It worked!")
 end
 
 
