05e6ffa3cac7b3b9ce2f6ddcdb3b3dc4901859a1
nykeztv <nykezgaming@live.com>
2018-03-19 01:55:13 -0400

    new vendor name-tags

--- entities/entities/npc_vendor/cl_init.lua
+++ entities/entities/npc_vendor/cl_init.lua
@@ -2,24 +2,53 @@ include("shared.lua");
 
 ENT.RenderGroup = RENDERGROUP_BOTH
 
+surface.CreateFont( "Fusion_NPC", {
+	font = "Bebas Neue",
+	extended = true,
+	size = ScreenScale(14),
+	weight = 0,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
 function ENT:Draw()
     if(LocalPlayer():GetPos():Distance(self:GetPos()) > 1000) then return end
 	
 	self:DrawModel()
+	self:DrawTranslucent()
+end
 
-	local ang = EyeAngles()
-	ang:RotateAroundAxis(ang:Right(),90)
-	ang:RotateAroundAxis(ang:Up(),-90)
 
-	local text = self:GetourName()
+function ENT:DrawTranslucent()
 	local color = Color(255, 255, 255)
 
-	cam.Start3D2D(self:GetPos() + Vector(0,0,75), ang,0.15)
-		surface.SetFont("TargetID")
-		local dX,dY = surface.GetTextSize(text)
 
+	local NPCNAME = self:GetourName()
+
+	local offset = Vector( 0, 0, 90 )
+
+	local ang = LocalPlayer():EyeAngles()
+	local pos = self:GetPos() + offset + ang:Up() * ( math.sin( CurTime() ) * 4 )
+
+	ang:RotateAroundAxis( ang:Forward(), 90 )
+	ang:RotateAroundAxis( ang:Right(), 90 )
+	local dX,dY = surface.GetTextSize(NPCNAME)
+
+
+	cam.Start3D2D( pos, Angle( 0, ang.y, 90 ), 0.14 )
 		surface.SetDrawColor(32,32,32,130)
 		surface.DrawRect(-(dX+32)/2,-dY-16,dX+32,dY+32)
-		draw.SimpleTextOutlined(text,"TargetID",0,-dY,color,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP,2,Color(25,25,25))
+
+		draw.DrawText( NPCNAME, "Fusion_NPC", 0, -dY, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+
 	cam.End3D2D()
 end
