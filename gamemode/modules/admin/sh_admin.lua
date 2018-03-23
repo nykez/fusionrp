ef5f090e22f4c9e2f2b5b69fc7be12a81aa871b2
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-03-22 22:27:59 -0500

    Scoreboard bs

--- gamemode/modules/admin/sh_admin.lua
+++ gamemode/modules/admin/sh_admin.lua
@@ -1,5 +1,25 @@
+FUSION_RANKS = {
+    Color(60, 60, 60),
+    Color(10, 10, 10),
+    Color(1, 1, 1),
+    Color(100, 255, 100),
+    Color(255, 100, 100)
+}
+
 function PLAYER:GetAccountLevel()
     if !IsValid(self) then return end
 
     return self:GetNetworkedInt("account_level", 0)
+end
+
+function PLAYER:GetRankColor()
+    if !IsValid(self) then return end
+
+    return FUSION_RANKS[self:GetAccountLevel()]
+end
+
+function PLAYER:IsStaff()
+    if !IsValid(self) then return end
+
+    return self:GetAccountLevel() >= 4
 end
--- gamemode/modules/scoreboard/cl_scoreboard_ui.lua
+++ gamemode/modules/scoreboard/cl_scoreboard_ui.lua
@@ -2,7 +2,7 @@ Fusion.scoreboard = Fusion.scoreboard or {}
 
 surface.CreateFont("Fusion_Scoreboard_Title", {
     font = "Bebas Neue",
-    size = ScreenScale(32),
+    size = 70,
     weight = 500,
     antialias = true
 })
@@ -20,15 +20,32 @@ function PANEL:Init()
     self:SetSize(ScrW(), ScrH())
     self:Center()
     self:ParentToHUD()
+    self:SetZPos(400)
+    self:SetMouseInputEnabled(true)
 
     self.panel = self:Add("DPanel")
     self.panel:SetSize(ScrW() * 0.5, ScrH() * 0.5)
     self.panel:SetPos(-self.panel:GetWide(), ScrH() / 2 - self.panel:GetTall() / 2)
     self.panel:TDLib():Background(Color(30, 30, 30)):Gradient(Color(38, 38, 38))
 
-    self.list = self.panel:Add("DScrollPanel")
+    self.title = self.panel:Add("DLabel")
+    self.title:SetFont("Fusion_Scoreboard_Title")
+    self.title:SetTextColor(color_white)
+    self.title:SetText("Fusion Roleplay")
+    self.title:Dock(TOP)
+    self.title:DockMargin(0, 0, 0, 0)
+    self.title:SizeToContents()
+    self.title:Center()
+    self.title:SetContentAlignment(2)
+
+    self.sub_panel = self.panel:Add("DPanel")
+    self.sub_panel:Dock(FILL)
+    self.sub_panel:DockMargin(5, 5, 5, 5)
+    self.sub_panel:TDLib():Background(Color(25, 25, 25))
+
+    self.list = self.sub_panel:Add("DScrollPanel")
     self.list:Dock(FILL)
-    self.list:DockMargin(6, 6, 6, 6)
+    self.list:DockMargin(8, 6, 8, 6)
     self.list:TDLib():HideVBar()
 
     self:Populate()
@@ -50,17 +67,30 @@ function PANEL:Populate()
         self.rows[k]:SetTall(60)
         self.rows[k]:Dock(TOP)
         self.rows[k]:DockMargin(0, 4, 0, 0)
+        self.rows[k]:TDLib():Background(Color(30, 30, 30))
+        self.rows[k].player = v
+        self.rows[k]:On("Think", function(s)
+            if s.player == LocalPlayer() then
+                s:Background(Color(36, 36, 36))
+            end
 
-        if v == LocalPlayer() then
-        	self.rows[k]:TDLib():Background(Color(64, 160, 255))
-        else
-        	self.rows[k]:TDLib():Background(Color(20, 20, 20))
-        end
+            if s.player:IsStaff() then
+                s:SideBlock(s.player:GetRankColor(), 4)
+            end
+        end)
+        self.rows[k]:On("OnCursorEntered", function(s)
+            s:SizeTo(-1, 75, 0.2, 0, 0.4, function() end)
+            s:Background(Color(36, 36, 36))
+        end)
+        self.rows[k]:On("OnCursorExited", function(s)
+            s:SizeTo(-1, 60, 0.2, 0, 0.4, function() end)
+            s:Background(Color(30, 30, 30))
+        end)
 
         local icon = self.rows[k]:Add("DPanel")
         icon:Dock(LEFT)
         icon:SetSize(46, 46)
-        icon:DockMargin(10, 4, 4, 4)
+        icon:DockMargin(14, 4, 4, 4)
         icon:TDLib():CircleAvatar():SetPlayer(v, 46)
 
         surface.SetFont("Fusion_Scoreboard_Name")
@@ -73,6 +103,30 @@ function PANEL:Populate()
         name:SetText(v:Nick())
         name:SetFont("Fusion_Scoreboard_Name")
         name:SetTextColor(color_white)
+    end
+end
+
+function PANEL:Clear()
+    for k, v in pairs(self.rows) do
+        v:Remove()
+    end
+end
+
+function PANEL:Think()
+    for k, v in pairs(self.rows) do
+        if !IsValid(v.player) then
+            self:Clear()
+            self:Populate()
+        end
+    end
+
+    if #self.rows != #player.GetAll() then
+        self:Clear()
+        self:Populate()
+    end
+
+    if input.IsMouseDown(MOUSE_RIGHT) then
+        gui.EnableScreenClicker(true)
     end
 end
 
@@ -91,5 +145,6 @@ function GM:ScoreboardHide()
     if Fusion.scoreboard.panel then
         Fusion.scoreboard.panel:Remove()
         Fusion.scoreboard.panel = nil
+        gui.EnableScreenClicker(false)
     end
 end
