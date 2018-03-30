61fd7cb5c6df95bddae76476f86ec0becf4f0cbc
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-03-30 10:54:47 -0500

    Removed the colossal hud and added temporary door text

--- gamemode/modules/hud/cl_hud.lua
+++ gamemode/modules/hud/cl_hud.lua
@@ -37,75 +37,60 @@ function GM:HUDDrawPickupHistory() return false; end
 local avatar
 
 local over = Material("gui/vignette.png")
-function DrawHUD()
-	if Fusion.property.panel then
-        avatar:SetVisible(false)
-        return
-    end
-
-    local bg = {}
-    bg.w = 400
-    bg.h = 200
-    bg.x = ScrW() - bg.w - 10
-    bg.y = ScrH() - bg.h - 10
-
-    surface.SetDrawColor(30, 30, 30)
-    surface.DrawRect(bg.x, bg.y, bg.w, bg.h)
-
-    local rect = {}
-    rect.w = 390
-    rect.h = bg.h - 60
-    rect.x = bg.x + 5
-    rect.y = bg.y + 60 - 5
-
-    surface.SetDrawColor(24, 24, 24)
-    surface.DrawRect(rect.x, rect.y, rect.w, rect.h)
-
-    if IsValid(LocalPlayer()) and !IsValid(avatar) then
-        avatar = vgui.Create("DPanel")
-        avatar:SetSize(48, 48)
-        avatar:SetPos(bg.x + 5, bg.y + 5)
-        avatar:TDLib():CircleAvatar():SetPlayer(LocalPlayer(), 45)
-    end
-
-    draw.SimpleText(LocalPlayer():GetRPName(), "Fusion_HUDFont", bg.x + bg.w - 10, bg.y + 4, Color(255, 255, 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
-
-    local hp = {}
-    hp.w = rect.w - 10
-    hp.h = (rect.h / 3) - 5
-    hp.x = rect.x + 5
-    hp.y = rect.y + 5
-
-    surface.SetDrawColor(140, 25, 25)
-    surface.DrawRect(hp.x, hp.y, hp.w, hp.h)
-
-    local stam = {}
-    stam.w = rect.w - 10
-    stam.h = (rect.h / 3) - 5
-    stam.x = rect.x + 5
-    stam.y = hp.y + hp.h + 5
-
-    surface.SetDrawColor(40, 180, 40)
-    surface.DrawRect(stam.x, stam.y, stam.w, stam.h)
-
-    local money = {}
-    money.w = rect.w / 2 - 5
-    money.h = (rect.h / 3) - 10
-    money.x = rect.x + rect.w / 2
-    money.y = stam.y + stam.h + 5
-
-    draw.RoundedBox(6, money.x, money.y, money.w, money.h, Color(16, 16, 16))
-
-    local ammo = {}
-    ammo.w = rect.w / 2 - 10
-    ammo.h = (rect.h / 3) - 10
-    ammo.x = rect.x + 5
-    ammo.y = stam.y + stam.h + 5
-
-    draw.RoundedBox(6, ammo.x, ammo.y, ammo.w, ammo.h, Color(16, 16, 16))
-
-    -- surface.SetDrawColor(255, 255, 255, 255)
-    -- surface.SetMaterial(over)
-    -- surface.DrawTexturedRect(0, 0, ScrW(), ScrH())
+local function DrawHUD()
+    //surface.SetDrawColor(255, 255, 255, 255)
+    //surface.SetMaterial(over)
+    //surface.DrawTexturedRect(0, 0, ScrW(), ScrH())
 end
-hook.Add("HUDPaint", "Fusion.HUDPaint", DrawHUD)
+hook.Add("HUDPaint", "Fusion.PaintHUD", DrawHUD)
+
+local function DoorHUD()
+	local trace = LocalPlayer():GetEyeTrace()
+
+	if IsValid(trace.Entity) and trace.Entity:IsDoor() then
+		local pos = trace.Entity:GetPos()
+		local name = nil
+		local id = nil
+		local isValidDoor = false
+
+        local cache = Fusion.property.cache
+
+		for k, v in pairs(cache) do
+			for _, dPos in pairs(v.doors) do
+				if dPos == pos and !v.government then
+					name = v.name
+					id = v.id
+					isValidDoor = true
+				end
+			end
+		end
+
+		if isValidDoor == true then
+			local x = trace.Entity:LocalToWorld(trace.Entity:OBBCenter()):ToScreen().x
+			local y = trace.Entity:LocalToWorld(trace.Entity:OBBCenter()):ToScreen().y
+
+			local dist = LocalPlayer():GetPos():Distance(pos)
+			local distM = dist / 40
+
+			draw.SimpleTextOutlined(name, "Fusion_HUDFont", x, y, Color(179, 124, 255, (255 - distM * 12.75)), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(20, 20, 20, (255 - distM * 12.75)))
+
+			local ownText = "For Rent"
+            local owners = Fusion.property.owners
+
+			if owners[id] == nil then
+				ownText = "For Rent"
+			elseif (IsValid(owners[id]) and owners[id]:IsPlayer()) then
+				if LocalPlayer():HasProperty(id) then
+					ownText = "Rented by You"
+				else
+					ownText = "Rented"
+				end
+			else
+				ownText = "Unknown"
+			end
+
+			draw.SimpleTextOutlined(ownText, "Fusion_HUDFont", x, y + 30, Color(255, 251, 98, (255 - distM * 12.75)), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(20, 20, 20, (255 - distM * 12.75)))
+		end
+	end
+end
+hook.Add("HUDPaint", "Fusion.PaintDoor", DoorHUD)
