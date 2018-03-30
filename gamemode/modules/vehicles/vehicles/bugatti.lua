583d26afc6fde57f15a56c3b1b6bd13efddf228b
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-03-30 10:56:46 -0500

    Test vehicles

---
+++ gamemode/modules/vehicles/vehicles/bugatti.lua
@@ -0,0 +1,16 @@
+
+local veh = {}
+
+veh.id = "bug_ss"
+
+veh.make = 'Bugatti'
+
+veh.name = "Veyron"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/bug_veyronss.mdl"
+
+veh.script = "scripts/vehicles/TDMCars/veyronss.txt"
+
+Fusion.vehicles:Register(veh)
