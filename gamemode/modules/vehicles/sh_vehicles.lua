0f88989e9e006c955e6a0276e03a444569eda0f5
pbrownsack <Parker@Parker-PC MINGW64 ~$ git git config --global user.email 36751134+pbrownsack@users.noreply.github.com>
2018-03-30 10:55:55 -0500

    Server/shared vehicles working

--- gamemode/modules/vehicles/sh_vehicles.lua
+++ gamemode/modules/vehicles/sh_vehicles.lua
@@ -1,12 +1,12 @@
 Fusion.vehicles = Fusion.vehicles or {}
 Fusion.vehicles.cache = Fusion.vehicles.cache or {}
+Fusion.vehicles.make = Fusion.vehicles.make or {}
 
+function Fusion.vehicles:Load()
+	local vehs = file.Find(GAMEMODE.FolderName.."/gamemode/modules/vehicles/vehicles/*.lua", "LUA")
 
-function Fusion.vehicles:LoadVehicles()
-	local cars = file.Find(GAMEMODE.FolderName.."/gamemode/modules/vehicles/cars/*.lua", "LUA")
-
-	for k, v in pairs(cars) do
-		local path = GAMEMODE.FolderName.."/gamemode/modules/vehicles/cars/"..v
+	for k, v in pairs(vehs) do
+		local path = GAMEMODE.FolderName.."/gamemode/modules/vehicles/vehicles/"..v
 
 
 		if SERVER then
@@ -15,19 +15,35 @@ function Fusion.vehicles:LoadVehicles()
 
 		include(path)
 	end
-	
+
 	MsgN("[Fusion RP] Loaded all vehicles")
 end
-hook.Add("PostGamemodeLoaded", "FusionRP.LoadVehicles", Fusion.vehicles.LoadVehicles)
+hook.Add("PostGamemodeLoaded", "FusionRP.LoadVehicles", Fusion.vehicles.Load)
 
-function Fusion.vehicles:RegisterVehicle(tblCar)
-	Fusion.vehicles.cache[tblCar.id] = tblCar
+function Fusion.vehicles:Register(tbl)
+	if !tbl.id then return end
+	Fusion.vehicles.cache[tbl.id] = tbl
+
+	if !Fusion.vehicles.make[tbl.make] then
+		Fusion.vehicles.make[tbl.make] = {}
+	end
+	
+	Fusion.vehicles.make[tbl.make][tbl.id] = tbl
 end
 
-function Fusion.vehicles:GetCarByID(ID)
+function Fusion.vehicles:GetTable(id)
 	return Fusion.vehicles.cache[id]
 end
 
-function Fusion.vehicles:GetAllCars()
-	return Fusion.vehicles.cache
+function Fusion.vehicles:GetAll()
+	return Fusion.vehicles.cache or {}
 end
+
+function ENTITY:GetVehicleOwner()
+	if !IsValid(self) then return end
+	if !self:IsVehicle() then return end
+
+	return self:GetNetworkedEntity("owner", nil)
+end
+
+hook.Add("OnReloaded", "Fusion.ReloadVehicles", Fusion.vehicles.Load)
