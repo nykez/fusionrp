b2188584a59ffa97f68b7ec6fe1e34c955453795
nykeztv <nykezgaming@live.com>
2018-04-02 15:13:05 -0400

    Disabled name plates for NPCs

--- entities/entities/npc_vendor/cl_init.lua
+++ entities/entities/npc_vendor/cl_init.lua
@@ -29,27 +29,27 @@ end
 
 
 function ENT:DrawTranslucent()
-	local color = Color(255, 255, 255)
+	-- local color = Color(255, 255, 255)
 
 
-	local NPCNAME = self:GetourName()
+	-- local NPCNAME = self:GetourName() or "nil"
 
-	local offset = Vector( 0, 0, 90 )
+	-- local offset = Vector( 0, 0, 90 )
 
-	local ang = LocalPlayer():EyeAngles()
-	local pos = self:GetPos() + offset + ang:Up() * ( math.sin( CurTime() ) * 4 )
+	-- local ang = LocalPlayer():EyeAngles()
+	-- local pos = self:GetPos() + offset + ang:Up() * ( math.sin( CurTime() ) * 4 )
 
-	ang:RotateAroundAxis( ang:Forward(), 90 )
-	ang:RotateAroundAxis( ang:Right(), 90 )
-	local dX,dY = surface.GetTextSize(NPCNAME)
+	-- ang:RotateAroundAxis( ang:Forward(), 90 )
+	-- ang:RotateAroundAxis( ang:Right(), 90 )
+	-- local dX,dY = surface.GetTextSize(NPCNAME or "nil")
 
 
-	cam.Start3D2D( pos, Angle( 0, ang.y, 90 ), 0.14 )
+	-- cam.Start3D2D( pos, Angle( 0, ang.y, 90 ), 0.14 )
 	
-		surface.SetDrawColor(32,32,32,130)
-		surface.DrawRect(-(dX+32)/2,-dY-16,dX+32,dY+32)
+	-- 	surface.SetDrawColor(32,32,32,130)
+	-- 	surface.DrawRect(-(dX+32)/2,-dY-16,dX+32,dY+32)
 
-		draw.DrawText( NPCNAME, "Fusion_NPC", 0, -dY, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+	-- 	draw.DrawText( NPCNAME, "Fusion_NPC", 0, -dY, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
 
-	cam.End3D2D()
+	-- cam.End3D2D()
 end
