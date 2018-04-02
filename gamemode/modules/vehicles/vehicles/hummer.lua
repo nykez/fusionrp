4a9ee55e5b85a85de5be3a5bb5f65e2a415aca6a
Huskeydude <john_vasko@hotmail.com>
2018-04-02 01:55:26 -0400

    Hummer now added

---
+++ gamemode/modules/vehicles/vehicles/hummer.lua
@@ -0,0 +1,66 @@
+local veh = {}
+
+veh.id = "hummer_hx"
+
+veh.make = 'Hummer'
+
+veh.name = "Hummer HX V8"
+
+veh.price = 15
+
+veh.model = "models/talonvehicles/hummerhx.mdl"
+
+veh.script = "scripts/vehicles/tah_humhx_v8.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "hummer_h3"
+
+veh.make = 'Hummer'
+
+veh.name = "Hummer H3 V8"
+
+veh.price = 15
+
+veh.model = "models/talonvehicles/tal_hummer_h3.mdl"
+
+veh.script = "scripts/vehicles/tah_h3_v8.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "hummer_h1"
+
+veh.make = 'Hummer'
+
+veh.name = "Hummer H1 Alpha"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/hummer_h1_tc.mdl"
+
+veh.script = "scripts/vehicles/lwcars/hummer_h1_tc.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "hummer_h1off"
+
+veh.make = 'Hummer'
+
+veh.name = "Hummer H1 Alpha Offroad Edition"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/hummer_h1_tc_offroad.mdl"
+
+veh.script = "scripts/vehicles/lwcars/hummer_h1_tc_offroad.txt"
+
+Fusion.vehicles:Register(veh)
