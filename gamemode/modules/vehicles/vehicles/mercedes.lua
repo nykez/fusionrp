03c8758d3a88dd0c77e1572bb9b46318e544f031
Huskeydude <john_vasko@hotmail.com>
2018-04-02 14:29:20 -0400

    Mercedes

---
+++ gamemode/modules/vehicles/vehicles/mercedes.lua
@@ -0,0 +1,168 @@
+local veh = {}
+
+veh.id = "mer_slsamg"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz SLS AMG"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/mer_slsamg.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/slsamg.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_sl65"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz SL 65"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/sl65amg.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/sl65amg.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_300sl"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz 300SL Gull Wing Coupe"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/mer_300slgull.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/mer300slgull.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_300mclaren"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz McLaren SLR"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/mer_slr.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/mer_slr.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_a45"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz A45"
+
+veh.price = 15
+
+veh.model = "models/r4_vehicles/mercedes/mercedes_a45.mdl"
+
+veh.script = "scripts/vehicles/r4_vehicles/mercedes_a45.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_gl450"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz GL-450"
+
+veh.price = 15
+
+veh.model = "models/skyautomotive/mercedes/mb_gl450.mdl"
+
+veh.script = "scripts/vehicles/skyautos/mb_gl450.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_svan"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz Sprinter Van"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/merc_sprinter_swb.mdl"
+
+veh.script = "scripts/vehicles/lwcars/merc_sprinter_swb.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_sbox"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz Sprinter Box Truck"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/merc_sprinter_boxtruck.mdl"
+
+veh.script = "scripts/vehicles/lwcars/merc_sprinter_swb.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_c63"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz C-63 AMG Black Series"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/mer_c63_amg.mdl"
+
+veh.script = "scripts/vehicles/lwcars/mer_c63_amg.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "mer_190e"
+
+veh.make = 'Mercedes-Benz'
+
+veh.name = "Mercedes-Benz 190E Evo II"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/mercedes_190e_evo.mdl"
+
+veh.script = "scripts/vehicles/lwcars/mercedes_190e_evo.txt"
+
+Fusion.vehicles:Register(veh)
