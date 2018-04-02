402b213b30e0d090d35304c3a53d195f49dc80dc
Huskeydude <john_vasko@hotmail.com>
2018-04-02 02:01:46 -0400

    shelby2

---
+++ gamemode/modules/vehicles/vehicles/shelby.lua
@@ -0,0 +1,49 @@
+local veh = {}
+
+veh.id = "shelby_cobra"
+
+veh.make = 'Shelby'
+
+veh.name = "Shelby Cobra 427 '65"
+
+veh.price = 15
+
+veh.model = "models/sentry/shelbycobra.mdl"
+
+veh.script = "scripts/vehicles/sentry/cobra427.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "shelby_500kr"
+
+veh.make = 'Shelby'
+
+veh.name = "Shelby GT500 KR"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/shelby_gt500kr.mdl"
+
+veh.script = "scripts/vehicles/lwcars/shelby_gt500kr.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "shelby_glhs"
+
+veh.make = 'Shelby'
+
+veh.name = "Shelby GLHS"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/shelby_glhs.mdl"
+
+veh.script = "scripts/vehicles/lwcars/shelby_glhs.txt"
+
+Fusion.vehicles:Register(veh)
