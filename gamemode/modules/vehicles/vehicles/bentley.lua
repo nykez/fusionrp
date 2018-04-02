5713a996926703f994daa3d243dc22097631da93
Huskeydude <john_vasko@hotmail.com>
2018-04-02 02:11:26 -0400

    bentelysoicansleepnow

---
+++ gamemode/modules/vehicles/vehicles/bentley.lua
@@ -0,0 +1,66 @@
+local veh = {}
+
+veh.id = "bent_cont"
+
+veh.make = 'Bentley'
+
+veh.name = "Bentley PM Continental GT"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/bently_pmcontinental.mdl"
+
+veh.script = "scripts/vehicles/lwcars/bently_pmcontinental.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "bent_blower"
+
+veh.make = 'Bentley'
+
+veh.name = "Bentley Bentley 4 1/2 Litre 'Blower'"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/bentley_blower.mdl"
+
+veh.script = "scripts/vehicles/lwcars/bentley_blower.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "bent_arnage"
+
+veh.make = 'Bentley'
+
+veh.name = "Bentley Arnage T"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/bentley_arnage_t.mdl"
+
+veh.script = "scripts/vehicles/lwcars/bentley_arnage.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "bent_exp"
+
+veh.make = 'Bentley'
+
+veh.name = "Bentley EXP 10"
+
+veh.price = 15
+
+veh.model = "models/whitetiger/bentleysp6.mdl"
+
+veh.script = "scripts/vehicles/whitetiger/bentleysp6.txt"
+
+Fusion.vehicles:Register(veh)
