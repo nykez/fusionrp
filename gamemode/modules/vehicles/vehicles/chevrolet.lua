129e3559e3b800de8908a960371a8096f910f4d8
Huskeydude <john_vasko@hotmail.com>
2018-04-27 19:28:01 -0400

    chevy aka shit lollmaorolfxd

---
+++ gamemode/modules/vehicles/vehicles/chevrolet.lua
@@ -0,0 +1,219 @@
+local veh = {}
+
+veh.id = "chevrolet_corvettec1"
+
+veh.make = 'Chevrolet'
+
+veh.name = "Corvette C1"
+
+veh.price = 15
+
+veh.model = "models/crsk_autos/chevrolet/corvette_c1_1957.mdl"
+
+veh.script = "scripts/vehicles/crsk_autos/crsk_chevrolet_corvette_c1_1957.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_camaross68"
+
+veh.make = 'Chevrolet'
+
+veh.name = "1968 Camaro SS"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/chev_camaro_68.mdl"
+
+veh.script = "scripts/vehicles/lwcars/chev_camaro_68.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_corvettec7"
+
+veh.make = 'Chevrolet'
+
+veh.name = "Corvette C7"
+
+veh.price = 15
+
+veh.model = "models/fusion/c7.mdl"
+
+veh.script = "scripts/vehicles/fusion/fusion_c7.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_impalass"
+
+veh.make = 'Chevrolet'
+
+veh.name = "1964 Impala SS"
+
+veh.price = 15
+
+veh.model = "models/sentry/impala.mdl"
+
+veh.script = "scripts/vehicles/sentry/impala.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_novass"
+
+veh.make = 'Chevrolet'
+
+veh.name = "1969 Nova SS"
+
+veh.price = 15
+
+veh.model = "models/fusion/novass.mdl"
+
+veh.script = "scripts/vehicles/fusion/fusion_novass.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_elcamino"
+
+veh.make = 'Chevrolet'
+
+veh.name = "El Camino SS"
+
+veh.price = 15
+
+veh.model = "models/sentry/elcamino.mdl"
+
+veh.script = "scripts/vehicles/sentry/elcamino.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_grandsport"
+
+veh.make = 'Chevrolet'
+
+veh.name = "Corvette Grand Sport"
+
+veh.price = 15
+
+veh.model = "models/sentry/96grandsport.mdl"
+
+veh.script = "scripts/vehicles/sentry/96grandsport.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_silverado"
+
+veh.make = 'Chevrolet'
+
+veh.name = "1500 Silverado"
+
+veh.price = 15
+
+veh.model = "models/sentry/silverado.mdl"
+
+veh.script = "scripts/vehicles/sentry/silverado.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_camaro17"
+
+veh.make = 'Chevrolet'
+
+veh.name = "2017 Camaro Zr1"
+
+veh.price = 15
+
+veh.model = "models/sentry/17camaro.mdl"
+
+veh.script = "scripts/vehicles/sentry/17camaro.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_belair"
+
+veh.make = 'Chevrolet'
+
+veh.name = "Belair"
+
+veh.price = 15
+
+veh.model = "models/sentry/belair.mdl"
+
+veh.script = "scripts/vehicles/sentry/belair.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_spark"
+
+veh.make = 'Chevrolet'
+
+veh.name = "Spark"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/spark.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/che_spark.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_stingray"
+
+veh.make = 'Chevrolet'
+
+veh.name = "Corvette Stingray 427"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/shev_stingray427.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/chevstingray427.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "chevrolet_chevelless"
+
+veh.make = 'Chevrolet'
+
+veh.name = "Chevelle SS"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/chevelless.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/che_chevelless.txt"
+
+Fusion.vehicles:Register(veh)
