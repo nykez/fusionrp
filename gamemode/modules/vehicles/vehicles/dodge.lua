9c49d54ee68117f807cbe79edd10adbd36e53a51
Huskeydude <john_vasko@hotmail.com>
2018-04-28 00:05:54 -0400

    dod

---
+++ gamemode/modules/vehicles/vehicles/dodge.lua
@@ -0,0 +1,134 @@
+local veh = {}
+
+veh.id = "dodge_demon"
+
+veh.make = 'Dodge'
+
+veh.name = "2018 Demon"
+
+veh.price = 15
+
+veh.model = "models/sentry/demon.mdl"
+
+veh.script = "scripts/vehicles/sentry/demon.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "dodge_3500"
+
+veh.make = 'Dodge'
+
+veh.name = "Ram 3500 Mega Cab Dually"
+
+veh.price = 15
+
+veh.model = "models/sentry/ram3500.mdl"
+
+veh.script = "scripts/vehicles/sentry/ram3500.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "dodge_monaco"
+
+veh.make = 'Dodge'
+
+veh.name = "Monaco"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/dodge_monaco.mdl"
+
+veh.script = "scripts/vehicles/lwcars/dodge_monaco.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "dodge_dart"
+
+veh.make = 'Dodge'
+
+veh.name = "1968 Dart Super Stock"
+
+veh.price = 15
+
+veh.model = "models/spedcars/dart68.mdl"
+
+veh.script = "scripts/vehicles/spedcars/dodge_dart_superstock.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "dodge_viper"
+
+veh.make = 'Dodge'
+
+veh.name = "Viper GTS ACR"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/dodge_viper.mdl"
+
+veh.script = "scripts/vehicles/lwcars/dod_viper.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "dodge_ram1500"
+
+veh.make = 'Dodge'
+
+veh.name = "Ram 1500 Outsdoors Man"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/dodge_ram_1500_outsdoorman.mdl"
+
+veh.script = "scripts/vehicles/lwcars/dodge_ram_1500_outdoorsman.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "dodge_daytona"
+
+veh.make = 'Dodge'
+
+veh.name = "Daytona Charger Hemi"
+
+veh.price = 15
+
+veh.model = "models/lonewolfie/dodge_daytona.mdl"
+
+veh.script = "scripts/vehicles/lwcars/dod_daytona.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "dodge_caravan"
+
+veh.make = 'Dodge'
+
+veh.name = "Grand Caravan"
+
+veh.price = 15
+
+veh.model = "models/sentry/grandcaravan.mdl"
+
+veh.script = "scripts/vehicles/sentry/caravan.txt"
+
+Fusion.vehicles:Register(veh)
