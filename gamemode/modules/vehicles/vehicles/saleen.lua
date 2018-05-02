458da8c5f349a8ed29b8a6034bfa3154fec8102d
Huskeydude <john_vasko@hotmail.com>
2018-05-02 12:37:55 -0400

    tdm uploaded cars so lets add them

---
+++ gamemode/modules/vehicles/vehicles/saleen.lua
@@ -0,0 +1,66 @@
+local veh = {}
+
+veh.id = "saleen_s28100"
+
+veh.make = 'Saleen'
+
+veh.name = "S281 2000"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/sal_s281_00.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/sal_s281_00.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "saleen_s281e06"
+
+veh.make = 'Saleen'
+
+veh.name = "S281E 2006"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/sal_s281_06.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/sal_s281e_06.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "saleen_s331"
+
+veh.make = 'Saleen'
+
+veh.name = "S331 Supercab"
+
+veh.price = 15
+
+veh.model = "models/tdmcars/sal_s331_supercab.mdl"
+
+veh.script = "scripts/vehicles/tdmcars/sal_s331_supercab.txt"
+
+Fusion.vehicles:Register(veh)
+
+
+local veh = {}
+
+veh.id = "saleen_s7"
+
+veh.make = 'Saleen'
+
+veh.name = "S7 Twin Turbo"
+
+veh.price = 15
+
+veh.model = "models/sentry/s7.mdl"
+
+veh.script = "scripts/vehicles/sentry/s7.txt"
+
+Fusion.vehicles:Register(veh)
