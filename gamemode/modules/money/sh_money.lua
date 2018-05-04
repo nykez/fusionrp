a31f8527314e28f3562b4e6fc115ebe36d215cd6
nykeztv <nykezgaming@live.com>
2018-05-03 20:40:09 -0400

    Vehicles converted to new data system

--- gamemode/modules/money/sh_money.lua
+++ gamemode/modules/money/sh_money.lua
@@ -15,6 +15,7 @@ if SERVER then
 
 		return true
 	end
+	characeter.AddMoney = character.giveMoney
 
 	function character:takeMoney(amount)
 		amount = math.abs(amount)
--- gamemode/modules/vehicles/sv_vehicles.lua
+++ gamemode/modules/vehicles/sv_vehicles.lua
@@ -5,79 +5,54 @@ Fusion.vehicles.make = Fusion.vehicles.make or {}
 util.AddNetworkString("Fusion.vehicles.sync")
 util.AddNetworkString("Fusion.vehicles.buy")
 
-function Fusion.vehicles:Purchase(ply, id, color)
-	if not ply then return end
-	if not id then return end
-
+function Fusion.vehicles.Purchase(pPlayer, id, color)
+	if not IsValid(pPlayer) then return end
+	
+	local character = pPlayer:getChar()
+	if not character then return end
+	
 	local veh = Fusion.vehicles:GetTable(id)
 	if not veh then return end
 
 	local price = veh.price
 
-	if ply:HasVehicle(id) then return end
-
-	if ply:GetBank() < price then return end
-
-	ply.vehicles[id] = {
+	if !character:HasMoney(price) then return end
+	
+	local vehicle_data = pPlayer:getData('vehicles', {})
+	vehicle_data[id] = {
 		color = color,
 		skin = 0,
 		bodygroups = {}
 	}
+	pPlayer:setData("vehicles", vehicle_data)
 
-	ply:TakeBank(price)
+	character:takeMoney(price)
 
-	ply:Notify("You bought the " .. veh.name .. " for $" .. price .. "!")
-
-	self:Sync(ply)
-	self:Save(ply)
+	pPlayer:Notify("You bought the " .. veh.name .. " for $" .. price .. "!")
 end
 
-function Fusion.vehicles:Sell(ply, id)
-	if !IsValid(ply) then return end
-	if !id then return end
+function Fusion.vehicles.Sell(pPlayer, id)
+	if not IsValid(pPlayer) then return end
+
+	local character = pPlayer:getChar()
+	if not character then return end
 
 	local veh = Fusion.vehicles:GetTable(id)
-	if !veh then return end
+	if not veh then return end
 
-	if !ply:HasVehicle(id) then return end
+	local data = character:getData("vehicles", {})
+	if not data[id] then return end
+	
+	// 30%
+	character:AddMoney(veh.price * 0.30)
+	data[id] = nil
 
-	ply:AddBank(math.Round(veh.price / 2))
+	character:setData("vehicles", data)
 
-	ply.vehicles[id] = nil
+	pPlayer:Notify("You have sold your vehicle.")
 
-	self:Sync(ply)
-	self:Save(ply)
 end
 
-function Fusion.vehicles:Sync(pPlayer)
-	net.Start("Fusion.vehicles.sync")
-		net.WriteTable(pPlayer.vehicles or {})
-	net.Send(pPlayer)
-end
-hook.Add("Fusion.PlayerLoaded", function(ply)
-	Fusion.vehicles:Sync(ply)
-end)
-
-function Fusion.vehicles:Save(pPlayer)
-	if not pPlayer.vehicles then
-		pPlayer.vehicles = {}
-	end
-
-	local data = Fusion.util:JSON(pPlayer.vehicles or {})
-
-	local updateObj = mysql:Update("player_data");
-		updateObj:Update("vehicles", data)
-		updateObj:Where("steam_id", pPlayer:SteamID())
-	updateObj:Execute();
-end
-
-net.Receive("Fusion.vehicles.buy", function(len, ply)
-	Fusion.vehicles:Purchase(ply, net.ReadString(), net.ReadTable())
-end)
-
-net.Receive("Fusion.vehicles.sell", function(len, ply)
-	Fusion.vehicles:Sell(ply, net.ReadInt(16))
-end)
 
 concommand.Add("printcars", function(ply, cmd, args)
 	PrintTable(ply.vehicles)
