1465fe88c0046867624bfa7f891f2c383514e274
Huskeydude <john_vasko@hotmail.com>
2018-05-05 19:30:25 -0400

    Saleen & Shelby plates done

--- gamemode/modules/vehicles/vehicles/saleen.lua
+++ gamemode/modules/vehicles/vehicles/saleen.lua
@@ -12,6 +12,12 @@ veh.model = "models/tdmcars/sal_s281_00.mdl"
 
 veh.script = "scripts/vehicles/tdmcars/sal_s281_00.txt"
 
+veh.lplate = {
+	Vec = Vector(0, -102.5, 47),
+	Ang = Angle(0, 0, 85),
+}
+
+
 Fusion.vehicles:Register(veh)
 
 
@@ -28,6 +34,11 @@ veh.price = 15
 veh.model = "models/tdmcars/sal_s281_06.mdl"
 
 veh.script = "scripts/vehicles/tdmcars/sal_s281e_06.txt"
+
+veh.lplate = {
+	Vec = Vector(0, -108, 43.5),
+	Ang = Angle(0, 0, 85),
+}
 
 Fusion.vehicles:Register(veh)
 
@@ -46,6 +57,11 @@ veh.model = "models/tdmcars/sal_s331_supercab.mdl"
 
 veh.script = "scripts/vehicles/tdmcars/sal_s331_supercab.txt"
 
+veh.lplate = {
+	Vec = Vector(0, -139.2, 25.5),
+	Ang = Angle(0, 0, 85),
+}
+
 Fusion.vehicles:Register(veh)
 
 
@@ -62,5 +78,10 @@ veh.price = 15
 veh.model = "models/sentry/s7.mdl"
 
 veh.script = "scripts/vehicles/sentry/s7.txt"
+
+veh.lplate = {
+	Vec = Vector(0, -126.5, 17),
+	Ang = Angle(0, 0, 85),
+}
 
 Fusion.vehicles:Register(veh)
--- gamemode/modules/vehicles/vehicles/shelby.lua
+++ gamemode/modules/vehicles/vehicles/shelby.lua
@@ -12,6 +12,11 @@ veh.model = "models/sentry/shelbycobra.mdl"
 
 veh.script = "scripts/vehicles/sentry/cobra427.txt"
 
+veh.lplate = {
+	Vec = Vector(0, -93.1, 16),
+	Ang = Angle(0, 0, 85),
+}
+
 Fusion.vehicles:Register(veh)
 
 
@@ -29,6 +34,11 @@ veh.model = "models/lonewolfie/shelby_gt500kr.mdl"
 
 veh.script = "scripts/vehicles/lwcars/shelby_gt500kr.txt"
 
+veh.lplate = {
+	Vec = Vector(0, -113.2, 24.5),
+	Ang = Angle(0, 0, 85),
+}
+
 Fusion.vehicles:Register(veh)
 
 
@@ -45,5 +55,10 @@ veh.price = 15
 veh.model = "models/lonewolfie/shelby_glhs.mdl"
 
 veh.script = "scripts/vehicles/lwcars/shelby_glhs.txt"
+
+veh.lplate = {
+	Vec = Vector(0, -94.5, 32.5),
+	Ang = Angle(0, 0, 85),
+}
 
 Fusion.vehicles:Register(veh)
