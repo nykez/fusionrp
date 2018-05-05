ef644ee6a9882db14f76e13c078895bc9bb6cb0a
nykeztv <nykezgaming@live.com>
2018-05-05 15:45:35 -0400

    renove old jobs

--- gamemode/modules/jobs/jobs/citizen.lua
+++ gamemode/modules/jobs/jobs/citizen.lua
@@ -1,28 +1,3 @@
-local job = {}
-
-job.id = 1
-
-job.name = "Citizen"
-
-job.color = Color(60, 255, 60)
-
-job.level = 0
-
-job.pay = 5
-
--- job.onJoin = function(pPlayer)
--- 	pPlayer:Notify("You have joined this job.")
-
--- 	pPlayer:StripWeapons()
-
--- end
-
--- join.onQuit = function(pPlayer)
--- 	pPlayer:Notify("You quit your job.")
-
--- end
-
-Fusion.jobs:Register(job)
 
 local JOB = {}
 JOB.ID = 1
