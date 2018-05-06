9206da859f7c6cb510fa0019a774d4f55e04c667
Huskeydude <john_vasko@hotmail.com>
2018-05-05 22:55:16 -0400

    bentley plates

--- gamemode/modules/vehicles/vehicles/bentley.lua
+++ gamemode/modules/vehicles/vehicles/bentley.lua
@@ -12,6 +12,11 @@ veh.model = "models/lonewolfie/bently_pmcontinental.mdl"
 
 veh.script = "scripts/vehicles/lwcars/bently_pmcontinental.txt"
 
+veh.lplate = {
+	Vec = Vector(0, -122.6, 31.5),
+	Ang = Angle(0, 0, 85),
+}
+
 Fusion.vehicles:Register(veh)
 
 
@@ -28,6 +33,11 @@ veh.price = 15
 veh.model = "models/lonewolfie/bentley_blower.mdl"
 
 veh.script = "scripts/vehicles/lwcars/bentley_blower.txt"
+
+veh.lplate = {
+	Vec = Vector(0, -91, 26),
+	Ang = Angle(0, 0, 70),
+}
 
 Fusion.vehicles:Register(veh)
 
@@ -46,6 +56,11 @@ veh.model = "models/lonewolfie/bentley_arnage_t.mdl"
 
 veh.script = "scripts/vehicles/lwcars/bentley_arnage.txt"
 
+veh.lplate = {
+	Vec = Vector(0, -114.22, 36),
+	Ang = Angle(0, 0, 70),
+}
+
 Fusion.vehicles:Register(veh)
 
 
@@ -62,5 +77,10 @@ veh.price = 15
 veh.model = "models/whitetiger/bentleysp6.mdl"
 
 veh.script = "scripts/vehicles/whitetiger/bentleysp6.txt"
+
+veh.lplate = {
+	Vec = Vector(0, -111, 22),
+	Ang = Angle(0, 0, 85),
+}
 
 Fusion.vehicles:Register(veh)
