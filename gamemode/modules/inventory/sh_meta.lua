5422d712d151616a30de050971556c82fdf5ad1b
nykeztv <nykezgaming@live.com>
2018-05-13 16:07:06 -0400

    inventory item meta

--- gamemode/modules/inventory/sh_meta.lua
+++ gamemode/modules/inventory/sh_meta.lua
@@ -49,4 +49,79 @@ function ITEM:getOwner()
 end
 
 
+function ITEM:setData(key, value, receivers, noSave, noCheckEntity)
+	self.data = self.data or {}
+	self.data[key] = value
+
+	if (SERVER) then
+		if (!noCheckEntity) then
+			local ent = self:getEntity()
+
+			if (IsValid(ent)) then
+				local data = ent:getNetVar("data", {})
+				data[key] = value
+
+				ent:setNetVar("data", data)
+			end
+		end
+	end
+
+	if (receivers != false) then
+		if (receivers or self:getOwner()) then
+			netstream.Start(receivers or self:getOwner(), "invData", self:getID(), key, value)
+		end
+	end
+
+	if (!noSave) then
+		// save inventory data here 
+	end	
+end
+
+function ITEM:getData(key, default)
+	self.data = self.data or {}
+
+	if (self.data) then
+		if (key == true) then
+			return self.data
+		end
+
+		local value = self.data[key]
+
+		if (value != nil) then
+			return value
+		elseif (IsValid(self.entity)) then
+			local data = self.entity:getNetVar("data", {})
+			local value = data[key]
+
+			if (value != nil) then
+				return value
+			end
+		end
+	else
+		self.data = {}
+	end
+
+	if (default != nil) then
+		return default
+	end
+
+	return
+end
+
+
+if SERVER then
+	function ITEM:getEntity()
+		local id = self:getID()
+
+		for k, v in ipairs(ents.FindByClass("ent_item")) do
+			if (v.ItemID == id) then
+				return v
+			end
+		end
+	end
+
+
+end
+
+
 Fusion.meta.item = ITEM
