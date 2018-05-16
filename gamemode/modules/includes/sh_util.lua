97ec1718697b457e0f2855ac829215856e83fb8e
nykeztv <nykezgaming@live.com>
2018-05-16 17:01:26 -0400

    prep for new inventory

--- gamemode/modules/includes/sh_util.lua
+++ gamemode/modules/includes/sh_util.lua
@@ -17,4 +17,64 @@ if SERVER then
 	end
 end
 
+-- Includes a file from the prefix.
+function Fusion.util.include(fileName, state)
+	if (!fileName) then
+		error("No file name specified for including.")
+	end
+
+	-- Only include server-side if we're on the server.
+	if ((state == "server" or fileName:find("sv_")) and SERVER) then
+		include(fileName)
+	-- Shared is included by both server and client.
+	elseif (state == "shared" or fileName:find("sh_")) then
+		if (SERVER) then
+			-- Send the file to the client if shared so they can run it.
+			AddCSLuaFile(fileName)
+		end
+
+		include(fileName)
+	-- File is sent to client, included on client.
+	elseif (state == "client" or fileName:find("cl_")) then
+		if (SERVER) then
+			AddCSLuaFile(fileName)
+		else
+			include(fileName)
+		end
+	end
+end
+
+-- Include files based off the prefix within a directory.
+function Fusion.util.includeDir(directory, fromLua, recursive)
+	local baseDir = "fusionrp"
+	
+	if (SCHEMA and SCHEMA.folder and SCHEMA.loading) then
+		baseDir = SCHEMA.folder.."/schema/"
+	else
+		baseDir = baseDir.."/gamemode/"
+	end 
+	
+	if recursive then
+		local function AddRecursive(folder)
+			local files, folders = file.Find(folder.."/*", "LUA")
+			if (!files) then MsgN("Warning! This folder is empty!") return end
+
+			for k, v in pairs(files) do
+				Fusion.util.include(folder .. "/" .. v)
+			end
+
+			for k, v in pairs(folders) do
+				AddRecursive(folder .. "/" .. v)
+			end
+		end
+		AddRecursive((fromLua and "" or baseDir)..directory)
+	else
+		-- Find all of the files within the directory.
+		for k, v in ipairs(file.Find((fromLua and "" or baseDir)..directory.."/*.lua", "LUA")) do
+			-- Include the file from the prefix.
+			Fusion.util.include(directory.."/"..v)
+		end
+	end
+end
+
 
